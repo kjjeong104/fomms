8af355b3b340833777e0a756b3eccb02ddd9fe60
Kyeong-jun Jeong <kjjeong104@gmail.com>
2018-07-19 15:50:23 -0500

    Add tests for trapezoidal and monte carlo integration

--- fomms_integrate/tests/test_fomms_integrate.py
+++ fomms_integrate/tests/test_fomms_integrate.py
@@ -5,8 +5,37 @@ Unit and regression test for the fomms_integrate package.
 # Import package, test suite, and other packages as needed
 import fomms_integrate
 import pytest
+import numpy as np
 import sys
 
 def test_fomms_integrate_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "fomms_integrate" in sys.modules
+
+"""
+Testing the integrate package
+"""
+
+def g(x):
+    return 3.0 * x
+
+def f(x):
+    return np.power(x, 2)
+
+def h(x):
+    return np.ones(x.size)
+
+def volume(x):
+    squares = np.power(x, 2)
+    return np.sum(squares, axis=1)
+
+def test_trapz():
+    x = np.array([0, 10])
+    I = fomms_integrate.newton_cotes.trapz(x, g)
+    assert I == 150.00
+
+def test_monte1d():
+    x = np.array([0, 3])
+    I = fomms_integrate.stochastic.monte_1d(x, f, 100000)
+    assert np.allclose(I, 9.00, 1e-2)
+
